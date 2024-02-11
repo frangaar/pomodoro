fb951a2e6e7a5427846242459ab8523f7f1ee9f5
Fran Garcia <francisco.garcia.arroyo@gmail.com>
2024-02-11 20:50:44 +0100

    Pomodoro

--- js/main.js
+++ js/main.js
@@ -1,46 +1,90 @@
 document.addEventListener('DOMContentLoaded',function(){
 
     let cols = document.querySelectorAll('.tablero .col');
+    let colsTasks = document.querySelectorAll('.tablero .col .tasksList');
+    
+    let taskData = new Array();
+
+    // let columnsHeight = document.querySelectorAll('.tablero .col')[0];
 
     cols.forEach((item)=>{
         item.style.height = window.innerHeight + 'px';
-    })
+    });
+    
+    colsTasks.forEach((item)=>{
+        item.style.height = (window.innerHeight - 120) + 'px';
+    });
 
-    let pendientes = document.getElementById('pendiente');
-    let curso = document.getElementById('curso');
-    let completados = document.getElementById('completado');
+    let pendientes = document.getElementById('save');
+    // let curso = document.getElementById('curso');
+    // let completados = document.getElementById('completado');
 
-    let eventos = [pendientes,curso,completados];
+    let eventos = [pendientes];
 
     eventos.forEach((boton) =>{
 
-        boton.querySelector('.addTask').addEventListener('click', function(){
+        boton.addEventListener('click', function(){
+
+            getFormData();
 
             let newTask = createTask();
 
-            this.parentElement.querySelector('.tasksList').appendChild(newTask);
+            document.querySelector('#pendiente .tasksList').appendChild(newTask);
         });
     });
 
+    let saveTask = document.getElementById('save');
 
+    saveTask.addEventListener('click',function(){
+
+        
+    });
+
+    function getFormData(){
+        let title = document.getElementById('title').value;
+        let description = document.getElementById('text').value;
+        
+        taskData['title'] = title;
+        taskData['desc'] = description;
+        // taskData['category'] = cat;
+    }
 
     function createTask(){
 
         let totalTasks = document.querySelectorAll('.tasksList > div').length;
 
-        let taskContainer = document.createElement('div');
-        taskContainer.setAttribute('class','taskContainer');
-        taskContainer.setAttribute('id',`task${totalTasks}`);
-        taskContainer.setAttribute('draggable','true');
-        taskContainer.setAttribute('ondragstart','drag(event)');
+        // let taskContainer = document.createElement('div');
+        // taskContainer.setAttribute('class','taskContainer');
+        // taskContainer.setAttribute('id',`task${totalTasks}`);
+        // taskContainer.setAttribute('draggable','true');
+        // taskContainer.setAttribute('ondragstart','drag(event)');
 
-        let singleTask = document.createElement('img');     
-        singleTask.setAttribute('src','img/img.png');
+        let singleTask = document.createElement('div');
+        singleTask.setAttribute('class','card mb-3');
+        singleTask.setAttribute('id',`task${totalTasks}`);
+        singleTask.setAttribute('draggable','true');
+        singleTask.setAttribute('ondragstart','drag(event)');
+
+        let body = document.createElement('div');
+        body.setAttribute('class','card-body');
+        let title = document.createElement('h5');
+        title.setAttribute('class','card-title')
+        title.innerText = 'Title: ' + taskData['title'];
+        let desc = document.createElement('p');
+        desc.setAttribute('class','card-text')
+        desc.innerText = 'Description: ' + taskData['desc'];
+
+        body.appendChild(title);
+        body.appendChild(desc);
+        singleTask.appendChild(body);
+
+        // let singleTask = document.createElement('img');     
+        // singleTask.setAttribute('src','img/img.png');
         
 
-        taskContainer.appendChild(singleTask);
+        // taskContainer.appendChild(singleTask);
 
-        return taskContainer;
+        return singleTask;
     }
 });
 
@@ -50,7 +94,7 @@ function allowDrop(ev) {
   }
   
   function drag(ev) {
-    ev.dataTransfer.setData("task", ev.target.parentElement.id);
+    ev.dataTransfer.setData("task", ev.target.id);
   }
   
   function drop(ev) {
